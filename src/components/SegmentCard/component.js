702e22e024ed987acacf669d45add623df5897c3
furkacz <100477457+furkacz@users.noreply.github.com>
2024-12-15 22:44:55 +0100

    Added score to segments in search

--- src/components/SegmentCard/component.js
+++ src/components/SegmentCard/component.js
@@ -7,6 +7,7 @@ export default {
   },
   props: {
     source: String,
+    score: Number,
     segment: String,
   },
 };
--- src/components/SegmentCard/index.vue
+++ src/components/SegmentCard/index.vue
@@ -2,6 +2,7 @@
   <div class="SegmentCard">
     <CollapsibleText class="SegmentCard__segment hover" :text="segment" :size="256" />
     <div class="SegmentCard__source">
+      <span class="SegmentCard__source--score">{{ score.toFixed(3) }}</span>
       <a class="SegmentCard__source--link hover" :href="source" target="_blank">{{ source }}</a>
     </div>
   </div>
